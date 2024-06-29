aed08ea23d6d0a80458715b21a0e3d0a9b91eb51
Fasakin Henry <fasakinhenry@gmail.com>
2024-06-30 00:30:22 +0100

    feat: add a pseudo data for the app notifications

---
+++ data/plans.js
@@ -0,0 +1,6 @@
+// data/plans.js
+module.exports = [
+  { day: 1, task: 'HTML basics: Create a basic HTML page.' },
+  { day: 2, task: 'HTML forms and media elements: Create a contact form.' },
+  // Add more daily plans as needed
+];
