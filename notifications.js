d4c9d7703f3cf0060252910a54e1bf5688a4cd34
Fasakin Henry <fasakinhenry@gmail.com>
2024-06-30 00:29:00 +0100

    feat: add notifcations.js to handle all the notification aspect of the app

---
+++ notifications.js
@@ -0,0 +1,25 @@
+const nodemailer = require('nodemailer');
+const config = require('./config');
+
+const transporter = nodemailer.createTransport({
+  service: config.email.service,
+  auth: config.email.auth,
+});
+
+function sendEmail(to, subject, message) {
+  const mailOptions = {
+    from: config.email.auth.user,
+    to,
+    subject,
+    text: message,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log('Error:', error);
+    }
+    console.log('Email sent:', info.response);
+  });
+}
+
+module.exports = { sendEmail };
